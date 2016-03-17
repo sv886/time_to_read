f2ebde1a91289af2486d269816f5dfda7b477cbe
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-03-16 21:35:53 -0500

    timeToRead function added. takes 2 params

--- time_to_read.js
+++ time_to_read.js
@@ -1,18 +1,27 @@
 $(window).ready(function(){
 
-  var werdz_count = document.getElementById('werdz').innerHTML.split(' ').length;
+  var werdzCount = document.getElementById('werdz').innerHTML.split(' ').length;
 
-  var werdzPerMinute = 230
+  var werdzPerMinute = 230;
+
+  var time = parseFloat(werdzCount/werdzPerMinute).toFixed(2)
+
+  // button
 
   $("button#count").on("click", function(){
-    alert("It will take an average of " + parseFloat(werdz_count/werdzPerMinute).toFixed(2) + " minutes to read these " + werdz_count + " werdz.");
+    alert("It will take an average of " + time + " minutes to read these " + werdzCount + " werdz.");
 
-  })
+  });
 
+  // function
 
-// timeToRead()
+  timeToRead = function(words, wpm){
 
+    alert(parseFloat(words/wpm).toFixed(2) + " minutes to read this page.");
 
+  };
+
+ timeToRead(werdzCount, werdzPerMinute)
 
 
 
