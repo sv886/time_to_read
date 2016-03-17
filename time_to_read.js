65be79aeb718068258e14c93f74aa6628e6aab17
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-03-16 20:09:07 -0500

    wired up cuz

---
+++ time_to_read.js
@@ -0,0 +1,17 @@
+$(window).ready(function(){
+
+  // var werdz_count = document.getElementById('werdz').innerHTML.split(' ').length;
+
+  $("button#count").on("click", function(){
+    alert("Yo!");
+
+  })
+
+
+// timeToRead()
+
+
+
+
+
+})
