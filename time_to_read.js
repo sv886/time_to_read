6d4d24cee2cff92b3aa00fd01ae9e9edd3ca208d
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-03-16 22:49:43 -0500

    alert at page load, button for 5 sec time to read show. needs work for multiple clicks

--- time_to_read.js
+++ time_to_read.js
@@ -2,27 +2,27 @@ $(window).ready(function(){
 
   var werdzCount = document.getElementById('werdz').innerHTML.split(' ').length;
 
-  var werdzPerMinute = 230;
-
-  var time = parseFloat(werdzCount/werdzPerMinute).toFixed(2)
-
-  // button
-
-  $("button#count").on("click", function(){
-    alert("It will take an average of " + time + " minutes to read these " + werdzCount + " werdz.");
-
-  });
-
   // function
 
   timeToRead = function(words, wpm = 230){
 
-    alert(parseFloat(words/wpm).toFixed(2) + " minutes to read this page.");
+    return parseFloat(words/wpm).toFixed(2);
 
   };
 
- timeToRead(werdzCount)
+  var readPage = timeToRead(werdzCount)
 
+  alert( readPage + " minutes to read this page.");
+
+
+  // button
+
+  $("button#count").on("click", function(){
+    $(".reading-time").append(readPage + " minutes to read these " + werdzCount + " werdz.");
+      setTimeout(function() {
+         $(".reading-time").fadeOut();
+     }, 5000);
+  });
 
 
 })
